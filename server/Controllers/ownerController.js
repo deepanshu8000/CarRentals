ab239033203bd8399897cda36ff5191f2c083f93
Deepanshu Dhakad <deepanshudhakad381@gmail.com>
2026-08-21 22:03:42 +0530

    Add image upload functionality and update owner routes
    - Implemented image upload to ImageKit in addCar API.
    - Updated ownerRoutes to include image upload middleware.
    - Created imageKit configuration file for ImageKit integration.

--- server/Controllers/ownerController.js
+++ server/Controllers/ownerController.js
@@ -1,27 +1,57 @@
+import path from "path";
+import imagekit from "../configs/imageKit.js";
+import Car from "../models/car.js"
 import User from "../models/user.js";
+import fs from "fs";
+
 // api to change role
-export const changeRoleToOwner= async(req,res)=>{
-    try{
-        const {_id}=req.user;
-        await User.findByIdAndUpdate(_id,{role:"owner"})
-        res.json({success:true,message:"Now, you can list a car"})
+export const changeRoleToOwner = async (req, res) => {
+    try {
+        const { _id } = req.user;
+        await User.findByIdAndUpdate(_id, { role: "owner" })
+        res.json({ success: true, message: "Now, you can list a car" })
     }
-    catch(error){
+    catch (error) {
         console.log(error.message)
-         res.json({success:false,message:error.message})
+        res.json({ success: false, message: error.message })
     }
 
 }
 // api to list car
-export const addCar=async(req,res)=>{
-    try{
-        const{_id}=req.user
-        let car=JSON.parse(req.body.carData);
-        const imageFile=req.file;
+export const addCar = async (req, res) => {
+    try {
+        const { _id } = req.user
+        let car = JSON.parse(req.body.carData);
+        const imageFile = req.file;
+        const fileBuffer = fs.readFileSync(imageFile.path)
 
-        
+        // upload image to imageKit
+        const response = await imagekit.upload({
+            file: fileBuffer,
+            fileName: imageFile.originalname,
+            folder: '/cars'
+        });
+        // URL generation
+        const optimizedImageUrl = imagekit.url({
+            path: response.filePath,
+            transformation: [
+                {
+                    width: 1280,
+                    crop: 'maintain_ratio',
+                    quality: 'auto', //Auto Compress
+                    format: 'webp', //convert to modern formats
+                },
+            ],
+        });
+
+        const image=optimizedImageUrl;
+        await Car.create({...car, owner: _id, image})
+        res.json({success:"true",message:"Car Added"})
+
+
+
     }
-    catch{
+    catch {
 
     }
 }
--- server/routes/ownerRoutes.js
+++ server/routes/ownerRoutes.js
@@ -1,7 +1,9 @@
 import express from "express";
 import protect from "../middleware/auth.js";
-import { changeRoleToOwner } from "../Controllers/ownerController.js";
+import { addCar, changeRoleToOwner } from "../Controllers/ownerController.js";
+import upload from "../middleware/multer.js";
 
 const ownerRouter=express.Router();
 ownerRouter.post("/change-role",protect,changeRoleToOwner)
+ownerRouter.post("/add-car",upload.single("image"),protect,addCar)
 export default ownerRouter;
